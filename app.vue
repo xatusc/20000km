5009cf61dea0d48a9e2d2af23b6647d0e1e70193
Claire Sersun <claire.sersun@gmail.com>
2026-01-18 10:14:48 -0500

    ok this should work

--- app.vue
+++ app.vue
@@ -2,19 +2,4 @@
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
-
-  <!-- Hidden form for Netlify Forms detection (required for SSR/client-rendered forms) -->
-  <form
-    name="contact"
-    data-netlify="true"
-    netlify-honeypot="bot-field"
-    hidden
-  >
-    <input type="hidden" name="form-name" value="contact">
-    <input name="bot-field">
-    <input name="name">
-    <input name="email">
-    <select name="reason"></select>
-    <textarea name="message"></textarea>
-  </form>
 </template>
